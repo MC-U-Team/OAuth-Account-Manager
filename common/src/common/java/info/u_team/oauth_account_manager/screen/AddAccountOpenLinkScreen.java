34345488cdab531f9b16b754823810b0ef3a44be
HyCraftHD <hycrafthd@live.de>
2023-04-28 00:40:09 +0200

    Added spinner

--- common/src/common/java/info/u_team/oauth_account_manager/screen/AddAccountOpenLinkScreen.java
+++ common/src/common/java/info/u_team/oauth_account_manager/screen/AddAccountOpenLinkScreen.java
@@ -1,21 +1,16 @@
 package info.u_team.oauth_account_manager.screen;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
 
-import info.u_team.oauth_account_manager.OAuthAccountManagerReference;
 import info.u_team.oauth_account_manager.init.OAuthAccountManagerLocalization;
 import info.u_team.u_team_core.gui.elements.UButton;
 import info.u_team.u_team_core.screen.UScreen;
-import info.u_team.u_team_core.util.RGBA;
-import info.u_team.u_team_core.util.RenderUtil;
 import net.minecraft.client.gui.components.MultiLineLabel;
 import net.minecraft.client.gui.layouts.FrameLayout;
 import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 
 public class AddAccountOpenLinkScreen extends UScreen {
 	
@@ -33,6 +28,9 @@ public class AddAccountOpenLinkScreen extends UScreen {
 		super.init();
 		
 		message = MultiLineLabel.create(font, Component.translatable(OAuthAccountManagerLocalization.SCREEN_ADD_ACCOUNT_OPEN_LINK_MESSAGE), width - 50);
+		
+		final LoadingSpinnerWidget spinner = addRenderableWidget(new LoadingSpinnerWidget(0, 0, 60, 60));
+		FrameLayout.centerInRectangle(spinner, 0, 0, width, height);
 		
 		final UButton openLink = addRenderableWidget(new UButton(0, 0, 100, 20, Component.translatable("chat.link.open")));
 		final UButton copyClipboard = addRenderableWidget(new UButton(0, 0, 100, 20, Component.translatable("chat.copy")));
@@ -53,46 +51,12 @@ public class AddAccountOpenLinkScreen extends UScreen {
 		return CommonComponents.joinForNarration(super.getNarrationMessage(), Component.translatable(OAuthAccountManagerLocalization.SCREEN_ADD_ACCOUNT_OPEN_LINK_MESSAGE));
 	}
 	
-	private int count;
-	
-	@Override
-	public void tick() {
-		super.tick();
-		count++;
-	}
-	
-	int rotation = 0;
-	
 	@Override
 	public void renderForeground(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
 		super.renderForeground(poseStack, mouseX, mouseY, partialTick);
 		
 		drawCenteredString(poseStack, font, title, width / 2, 20, 0xFFFFFF);
 		message.renderCentered(poseStack, this.width / 2, 60);
-		
-		poseStack.pushPose();
-		
-		poseStack.translate(25, 25, 0);
-		
-		if (count % 20 == 0) {
-			rotation += 30;
-		}
-		
-		poseStack.mulPose(Axis.ZP.rotationDegrees(rotation));
-		poseStack.translate(-25, -25, 0);
-		// poseStack.mulPose(Axis.YP.rotationDegrees(60));
-		
-		RenderUtil.drawTexturedQuad(poseStack, 0, 0 + 50, 0, 0 + 50, 0, 1, 0, 1, 0, new ResourceLocation(OAuthAccountManagerReference.MODID, "textures/gui/spinner.png"), RGBA.WHITE);
-		
-		// RenderSystem.setShader(GameRenderer::getPositionColorShader);
-		// BufferBuilder bufferbuilder = Tesselator.getInstance().getBuilder();
-		// bufferbuilder.begin(VertexFormat.Mode.TRIANGLES, DefaultVertexFormat.POSITION);
-		// bufferbuilder.vertex(poseStack.last().pose(), -0.5f, -0.5f, 0).color(0xFFFFFFFF).endVertex();
-		// bufferbuilder.vertex(poseStack.last().pose(), 0.5f, -0.5f, 0).color(0xFFFFFFFF).endVertex();
-		// bufferbuilder.vertex(poseStack.last().pose(), 0, 0.5f, 0).color(0xFFFFFFFF).endVertex();
-		// BufferUploader.drawWithShader(bufferbuilder.end());
-		
-		poseStack.popPose();
 	}
 	
 	@Override
---
+++ common/src/common/java/info/u_team/oauth_account_manager/screen/LoadingSpinnerWidget.java
@@ -0,0 +1,71 @@
+package info.u_team.oauth_account_manager.screen;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+
+import info.u_team.oauth_account_manager.OAuthAccountManagerReference;
+import info.u_team.u_team_core.api.gui.PerspectiveRenderable;
+import info.u_team.u_team_core.util.RGBA;
+import info.u_team.u_team_core.util.RenderUtil;
+import net.minecraft.Util;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.resources.ResourceLocation;
+
+public class LoadingSpinnerWidget extends AbstractWidget implements PerspectiveRenderable {
+	
+	private static final ResourceLocation SPINNER = new ResourceLocation(OAuthAccountManagerReference.MODID, "textures/gui/spinner.png");
+	
+	private long lastTime;
+	private int currentRotation;
+	
+	public LoadingSpinnerWidget(int x, int y, int width, int height) {
+		super(x, y, width, height, CommonComponents.EMPTY);
+	}
+	
+	@Override
+	public void renderWidget(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
+		renderBackground(poseStack, mouseX, mouseY, partialTick);
+		renderForeground(poseStack, mouseX, mouseY, partialTick);
+	}
+	
+	@Override
+	public void renderBackground(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
+		poseStack.pushPose();
+		
+		poseStack.translate(getX(), getY(), 0);
+		
+		final float xRot = width / 2f;
+		final float yRot = height / 2f;
+		
+		poseStack.translate(xRot, yRot, 0);
+		poseStack.mulPose(Axis.ZP.rotationDegrees(currentRotation));
+		poseStack.translate(-xRot, -yRot, 0);
+		
+		RenderUtil.drawTexturedQuad(poseStack, 0, 0 + width, 0, 0 + height, 0, 1, 0, 1, 0, SPINNER, RGBA.WHITE);
+		
+		poseStack.popPose();
+		
+		if (Util.getMillis() - lastTime > 10) {
+			lastTime = Util.getMillis();
+			currentRotation += 2;
+		}
+	}
+	
+	@Override
+	public void renderForeground(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
+	}
+	
+	@Override
+	protected void updateWidgetNarration(NarrationElementOutput element) {
+		defaultButtonNarrationText(element);
+	}
+	
+	@Override
+	public void playDownSound(SoundManager pHandler) {
+		// Don't play click sound
+	}
+	
+}
