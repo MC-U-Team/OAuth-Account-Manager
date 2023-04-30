4d0041f8e833ed38d381fa9867b0fdbe4ae3e500
HyCraftHD <hycrafthd@live.de>
2023-04-30 12:43:47 +0200

    Rename keys and generate language

--- common/src/common/java/info/u_team/oauth_account_manager/init/OAuthAccountManagerLocalization.java
+++ common/src/common/java/info/u_team/oauth_account_manager/init/OAuthAccountManagerLocalization.java
@@ -15,14 +15,14 @@ public class OAuthAccountManagerLocalization {
 	public static final String SCREEN_ADD_ACCOUNT_INFORMATION_SPINNER_TOOLTIP_WAITING = "screen.oauthaccountmanager.add_account_information.spinner.tooltip.waiting";
 	public static final String SCREEN_ADD_ACCOUNT_INFORMATION_SPINNER_TOOLTIP_LOGGING = "screen.oauthaccountmanager.add_account_information.spinner.tooltip.logging";
 	
-	public static final String SCREEN_GENERAL_LOGIN_STATE_INFO_INITAL_FILE = "screen.oauthaccountmanager.general.login_state_info.inital_file";
-	public static final String SCREEN_GENERAL_LOGIN_STATE_INFO_LOGIN_MICOSOFT = "screen.oauthaccountmanager.general.login_state_info.login_microsoft";
-	public static final String SCREEN_GENERAL_LOGIN_STATE_INFO_XBL_TOKEN = "screen.oauthaccountmanager.general.login_state_info.xbl_token";
-	public static final String SCREEN_GENERAL_LOGIN_STATE_INFO_XSTS_TOKEN = "screen.oauthaccountmanager.general.login_state_info.xsts_token";
-	public static final String SCREEN_GENERAL_LOGIN_STATE_INFO_ACCESS_TOKEN = "screen.oauthaccountmanager.general.login_state_info.access_token";
-	public static final String SCREEN_GENERAL_LOGIN_STATE_INFO_ENTITLEMENT = "screen.oauthaccountmanager.general.login_state_info.entitlement";
-	public static final String SCREEN_GENERAL_LOGIN_STATE_INFO_PROFILE = "screen.oauthaccountmanager.general.login_state_info.profile";
-	public static final String SCREEN_GENERAL_LOGIN_STATE_INFO_XBOX_PROFILE = "screen.oauthaccountmanager.general.login_state_info.xbox_profile";
+	public static final String SCREEN_ACOUNT_LOGIN_LOGIN_STATE_INFO_INITAL_FILE = "screen.oauthaccountmanager.account_login.login_state_info.inital_file";
+	public static final String SCREEN_ACOUNT_LOGIN_LOGIN_STATE_INFO_LOGIN_MICOSOFT = "screen.oauthaccountmanager.account_login.login_state_info.login_microsoft";
+	public static final String SCREEN_ACOUNT_LOGIN_LOGIN_STATE_INFO_XBL_TOKEN = "screen.oauthaccountmanager.account_login.login_state_info.xbl_token";
+	public static final String SCREEN_ACOUNT_LOGIN_LOGIN_STATE_INFO_XSTS_TOKEN = "screen.oauthaccountmanager.account_login.login_state_info.xsts_token";
+	public static final String SCREEN_ACOUNT_LOGIN_LOGIN_STATE_INFO_ACCESS_TOKEN = "screen.oauthaccountmanager.account_login.login_state_info.access_token";
+	public static final String SCREEN_ACOUNT_LOGIN_LOGIN_STATE_INFO_ENTITLEMENT = "screen.oauthaccountmanager.account_login.login_state_info.entitlement";
+	public static final String SCREEN_ACOUNT_LOGIN_LOGIN_STATE_INFO_PROFILE = "screen.oauthaccountmanager.account_login.login_state_info.profile";
+	public static final String SCREEN_ACOUNT_LOGIN_LOGIN_STATE_INFO_XBOX_PROFILE = "screen.oauthaccountmanager.account_login.login_state_info.xbox_profile";
 	
 	public static final String SCREEN_MULTIPLAYER_ADDITION_ACCOUNT_BUTTON = "screen.oauthaccountmanager.multiplayer_addition.account_button";
 	public static final String SCREEN_MULTIPLAYER_ADDITION_ACCOUNT_BUTTON_TOOLTIP_CHECKING = "screen.oauthaccountmanager.multiplayer_addition.account_button.tooltip.checking";
